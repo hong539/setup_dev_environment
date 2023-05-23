6b30de23dea89642c1472e9665605221dbe1822c
hong <j50508@gmail.com>
2023-05-24 02:21:45 +0800

    Up codes

--- shell/bash/src/find_and_file.sh
+++ shell/bash/src/find_and_file.sh
@@ -7,3 +7,7 @@ find /path/to/search -type f -exec file -i {} \; | grep -i 'text\|xml\|html\|pdf
 # ! -executable
 # formant ASCII
 find /path/to/search -type f -size 1033c ! -executable -exec file {} + | grep ASCII
+
+find / -user arch -group dev -size 1033c 2>/dev/null
+
+grep sogood log.txt | cut -d ' ' -f 2
