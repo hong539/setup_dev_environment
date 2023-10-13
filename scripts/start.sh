1e2c8c3dcb4b17934a6581ea80128fe152a5964f
hong <j50508@gmai.com>
2023-10-13 01:22:36 +0000

    [skip ci] update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#pull latest
+docker pull focal1119/setup_dev_environment:latest
+
 #stop current container
-docker setup_dev_environment
+docker stop setup_dev_environment
 
 #remove current container
 docker rm setup_dev_environment
