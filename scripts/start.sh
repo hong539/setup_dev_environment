f1dbe24c6e6e3372215e1f0082a86bce2872e5a1
Hong <hong@ubuntu-lab-01>
2024-09-02 21:31:41 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,4 +9,4 @@ docker stop setup_dev_environment
 #remove current container
 docker rm setup_dev_environment
 
-docker run -d --name setup_dev_environment -p 8000:80 docker.io/focal1119/setup_dev_environment:latest
+docker run --restart=always -d --name setup_dev_environment -p 8000:80 docker.io/focal1119/setup_dev_environment:latest
