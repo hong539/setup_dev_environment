103edb73ecc7507adf819efade2e0e53fc23c57f
hong539 <yunghunghuang984@gmail.com>
2025-08-02 16:34:42 +0800

    modified:   virtualbox/guest_os_time_sync.sh

--- virtualbox/guest_os_time_sync.sh
+++ virtualbox/guest_os_time_sync.sh
@@ -2,6 +2,10 @@
 set -euxo pipefail
 
 #For RHEL/Rocky Linux/CentOS/...etc to get VirtualBox support from host OS
+#docs: https://www.virtualbox.org/manual/ch02.html#externalkernelmodules
+#docs: https://www.virtualbox.org/manual/ch04.html#additions-linux
+#downloads: https://download.virtualbox.org/virtualbox/7.1.10/
 sudo dnf install -y epel-release
 sudo dnf groupinstall -y "Development Tools"
-sudo dnf install -y kernel-devel kernel-headers dkms elfutils-libelf-devel qt5-qtbase-devel
+sudo dnf install -y kernel-devel kernel-headers dkms
+# sudo dnf install -y kernel-devel kernel-headers dkms elfutils-libelf-devel qt5-qtbase-devel
