07cab01fd3ec24e4bd9f12fb913f23c22f9194c6
hong539 <yunghunghuang984@gmail.com>
2025-08-02 19:57:01 +0800

    [skip ci] modified:   virtualbox/guest_time_sync.sh

--- virtualbox/guest_time_sync.sh
+++ virtualbox/guest_time_sync.sh
@@ -9,3 +9,6 @@ systemctl start vboxadd-service.service
 
 #hint :
 #makesure which device is right for vboxadd-service.service?
+
+#debug with virtualbox Guest Additions and Chrony NTP client/server
+dmesg | egrep -i "timesync|virtualbox"
