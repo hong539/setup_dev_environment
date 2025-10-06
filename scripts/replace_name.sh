50987a9a55128672bf1a2cebddd2e55279d0b28b
hong539 <yunghunghuang984@gmail.com>
2025-10-06 22:20:33 +0800

    Update scripts/replace_name.sh

--- scripts/replace_name.sh
+++ scripts/replace_name.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-find ~/repos/setup_devops_environment -type f -exec sed -i 's/setup_dev_environment/setup_devops_environment/g' {} +
-find ~/repos/setup_devops_environment -type f -exec sed -i 's/hong539/blackdesert575/g' {} +
+find ~/repos/setup_devops_environment -type f -exec sed -i 's/setup_devops_environment/setup_devops_environment/g' {} +
+find ~/repos/setup_devops_environment -type f -exec sed -i 's/blackdesert575/blackdesert575/g' {} +
